102a001b0653c2b47a7a7a244ca09eca9a8978c2
Allister Driscoll <115250579+allisterdrisc@users.noreply.github.com>
2025-08-28 13:46:42 -0400

    Update DocsPage.tsx to use prose invert for dark mode

--- src/components/docs/DocsPage.tsx
+++ src/components/docs/DocsPage.tsx
@@ -5,11 +5,12 @@ import docsContent from "../../assets/content/sections/09-docs";
 export default function DocsPage() {
   const darkModeOn = useDarkModeOn();
   const backgroundColor = darkModeOn ? ' bg-[#01233E]' : 'bg-amber-10';
+  const textStyles = darkModeOn ? 'prose-invert' : '';
 
   return (
     <div className={`min-h-screen ${backgroundColor}`}>
       <div className="pt-16 px-8 md:px-16 lg:px-24 xl:px-32 max-w-6xl mx-auto">
-        <div className="prose prose-lg max-w-none md">
+        <div className="prose prose-lg max-w-none ${textStyles}">
           <ReactMarkdown>
             {docsContent}
           </ReactMarkdown>
