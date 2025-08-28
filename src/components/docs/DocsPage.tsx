c87a5c3ab159fff8f6d132a5d8f95c2b59157e24
Allister Driscoll <115250579+allisterdrisc@users.noreply.github.com>
2025-08-28 14:57:41 -0400

    Update DocsPage.tsx to fix dark mode text color

--- src/components/docs/DocsPage.tsx
+++ src/components/docs/DocsPage.tsx
@@ -6,13 +6,14 @@ import docsContent from "../../assets/content/sections/09-docs";
 export default function DocsPage() {
   const darkModeOn = useDarkModeOn();
   const backgroundColor = darkModeOn ? ' bg-[#01233E]' : 'bg-amber-10';
+  const textStyles = darkModeOn ? 'prose-invert' : '';
   const components: { [key: string]: React.ComponentType } = {
     code: CodeBlock,
   };
   return (
     <div className={`min-h-screen ${backgroundColor}`}>
       <div className="pt-16 px-8 md:px-16 lg:px-24 xl:px-32 max-w-6xl mx-auto">
-        <div className="prose prose-lg max-w-none">
+        <div className={`prose prose-lg max-w-none ${textStyles}`}>
           <ReactMarkdown components={components}>
             {docsContent}
           </ReactMarkdown>
