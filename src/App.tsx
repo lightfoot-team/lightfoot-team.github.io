ce7c36b840cb5444ed20d01715bba81134d77333
Allister Driscoll <115250579+allisterdrisc@users.noreply.github.com>
2025-08-28 13:23:41 -0400

    Update App.tsx to include docs route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useDarkModeOn } from './hooks/useDarkModeOn';
 import TopNavBar from './components/TopNavBar';
 import OurTeam from './components/team/OurTeam';
 import SplashPage from './components/splash/SplashPage';
+import DocsPage from './components/docs/DocsPage';
 import CaseStudyLayout from './components/case study/CaseStudyLayout';
 export default function App() {
   const darkModeOn = useDarkModeOn(); 
@@ -16,6 +17,7 @@ export default function App() {
       <Routes>
           <Route index element={<SplashPage></SplashPage>} />
           <Route path="our-team" element={<OurTeam></OurTeam>} />
+          <Route path="docs" element={<DocsPage></DocsPage>} />
           <Route path="case-study" element={<CaseStudyLayout></CaseStudyLayout>}  />
       
       </Routes>
